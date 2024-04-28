cb116176e066de66b28a712c84c604bd5c580fe8
Carlox <carlito1999@live.com>
2024-04-27 22:09:24 -0500

    formulario basiquito de momento

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,26 +1,115 @@
-import React from 'react';
+import { useState } from "react";
+import PropTypes from 'prop-types';
 
-function Register() {
+const InputField = ({ label, type, id, placeholder, value, onChange }) => (
+  <div className="mb-4">
+    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={id}>
+      {label}
+    </label>
+    <input
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      id={id}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  </div>
+);
+
+InputField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+    lastname: "",
+    phoneNumber: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // aqui me falta poner toda la logica :'v
+    console.log(formData);
+  };
+
   return (
-    <div>
-      <h2>Registro</h2>
-      <form>
-        <div>
-          <label htmlFor="nombre">Nombre:</label>
-          <input type="text" id="nombre" />
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <InputField
+          label="Nombre"
+          type="text"
+          id="name"
+          placeholder="Nombre"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <InputField
+          label="Apellido"
+          type="text"
+          id="lastName"
+          placeholder="Apellido"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+        <InputField
+          label="Número de Teléfono"
+          type="tel"
+          id="phoneNumber"
+          placeholder="123-456-7890"
+          value={formData.phoneNumber}
+          onChange={handleChange}
+        />
+        <InputField
+          label="Correo Electrónico"
+          type="email"
+          id="email"
+          placeholder="Correo Electrónico"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <InputField
+          label="Contraseña"
+          type="password"
+          id="password"
+          placeholder="******************"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <InputField
+          label="Confirmar Contraseña"
+          type="password"
+          id="confirmPassword"
+          placeholder="******************"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+        />
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Registrarse
+          </button>
         </div>
-        <div>
-          <label htmlFor="email">Correo electrónico:</label>
-          <input type="email" id="email" />
-        </div>
-        <div>
-          <label htmlFor="contraseña">Contraseña:</label>
-          <input type="password" id="contraseña" />
-        </div>
-        <button type="submit">Registrarse</button>
       </form>
     </div>
   );
-}
+};
 
 export default Register;
