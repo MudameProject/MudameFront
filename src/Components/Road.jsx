398d8c6fcaf4d89d132e46337dbb49fdeba23248
Samuel Rodriguez <samuelrodriguez795@gmail.com>
2024-04-30 12:35:31 -0500

    animacion de linea perrona beta

--- src/Components/Road.jsx
+++ src/Components/Road.jsx
@@ -3,20 +3,18 @@ import Truck from "./Truck";
 import YellowLines from "./YellowLines";
 
 function Road() {
-  
-  const calle = document.getElementById("calle");
-
+  const tamaño = screen.height;
 
   return (
-    <div className="flex w-full h-32 absolute left-0 bottom-0 bg-zinc-800 z-10">
-        <Truck/>
-        <div id="calle" className="w-full h-full flex justify-around pt-12 ">
-          <YellowLines/>
-          <YellowLines/>
-          <YellowLines/>
-          <YellowLines/>
-          <YellowLines/>
-        </div>
+    <div className="flex w-[120%] h-32 absolute left-0 bottom-0 bg-zinc-800 z-10">
+      <Truck />
+      <div className="w-full h-full absolute flex justify-around pt-12 ">
+          <YellowLines />
+          <YellowLines />
+          <YellowLines />
+          <YellowLines />
+          <YellowLines />
+      </div>
     </div>
   );
 }
--- src/Components/YellowLines.jsx
+++ src/Components/YellowLines.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import "./../App.css"
 
 export default function YellowLines() {
   return (
-    <div className='bg-yellow-600 w-24 h-4'>
+    <div className='bg-yellow-600 w-24 h-4 move-left-to-right'>
       
     </div>
   )
