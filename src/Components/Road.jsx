92eff6a23d09878ca59a6a86cc97995f6def5385
daalosx <daalosx@gmail.com>
2024-05-21 04:49:52 +0000

    subida definitiva

--- src/Components/Road.jsx
+++ src/Components/Road.jsx
@@ -4,9 +4,13 @@ import UpperRoad from "./UpperRoad";
 
 function Road() {
   return (
-    <>
-      <UpperRoad/>
-    </>
+    <div className="w-screen h-[90vh] absolute left-0 bottom-0 z-10 overflow-hidden">
+      <div className="doble"></div>
+      <div className="otraM"></div>
+      <div className="mountains"></div>
+      <Truck />
+      <div className="infinite"></div>
+    </div>
   );
 }
 export default Road;
