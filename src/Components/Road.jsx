52e252835d623b1501c7e89cbdd6d4127162558e
daalosx <daalosx@gmail.com>
2024-05-21 04:46:43 +0000

    carretera desde arriva

--- src/Components/Road.jsx
+++ src/Components/Road.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Truck from "./Truck";
+import UpperRoad from "./UpperRoad";
 
 function Road() {
   return (
-    <div className="w-screen h-[90vh] absolute left-0 bottom-0 z-10 overflow-hidden">
-      <div className="doble"></div>
-      <div className="otraM"></div>
-      <div className="mountains"></div>
-      <Truck />
-      <div className="infinite"></div>
-    </div>
+    <>
+      <UpperRoad/>
+    </>
   );
 }
 export default Road;
+
